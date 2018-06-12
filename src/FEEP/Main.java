c97de0c94e98c14e5ec66ed0a8bec8a6fb4fa86c
Alexander Simeonov <troqta@gmail.com>
2018-06-12 15:41:50 +0300

    Initial commit

---
+++ src/FEEP/Main.java
@@ -0,0 +1,109 @@
+package FEEP;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        startMenu();
+    }
+
+    private static void startMenu() {
+
+
+        int option = 0;
+        while (option <= 0 || option > 2) {
+
+            System.out.println("Enter an option!");
+            System.out.println("1: Expression to result.");
+            System.out.println("2: Exit");
+            option = in.nextInt();
+
+            switch (option) {
+                case 1:
+                    expressionToResult();
+                    break;
+                case 2:
+                    return;
+            }
+        }
+    }
+
+    private static void expressionToResult() {
+        System.out.println("Enter the expression : ");
+        in.nextLine();
+        String expression = in.nextLine();
+
+        System.out.println(eval(expression));
+        startMenu();
+    }
+
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char) ch);
+                return x;
+            }
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (; ; ) {
+                    if (eat('+')) x += parseTerm();
+                    else if (eat('-')) x -= parseTerm();
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (; ; ) {
+                    if (eat('*')) x *= parseFactor();
+                    else if (eat('/')) x /= parseFactor();
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor();
+                if (eat('-')) return -parseFactor();
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) {
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') {
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+
+                return x;
+            }
+        }.parse();
+    }
+}
